800a283bbf3d5e431045467d0a81d67e3ea927ef
matiscava <matiscava@hotmail.com>
2022-03-10 10:16:08 -0300

    clase 45

---
+++ clase45-app/start/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+// Route.on('/').render('welcome')
+
+Route.get('/', () => 'Hello world from Adonis')
+// Route.get('/portfolio', 'Portfolio ')
+
+const Cupcake = use('App/Models/Cupcake')
+
+Route
+  .get('cupcake', async() => {
+    return await Cupcake.all()
+  })
+
+  Route
+    .get('sin-controller', ({request}) => {
+      const params = request.get();
+      return {params};
+    })
